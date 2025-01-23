30685ebf45513d17eb2113ffb371ac4521436b26
B4R3BUH <157827248+B4R3BUH@users.noreply.github.com>
2025-01-23 23:19:22 +0300

    Update config.py

--- config.py
+++ config.py
@@ -1,5 +1,5 @@
-db_host = 'FVH1.spaceweb.ru'
-db_port = 3306
-db_user = 'reichstag1'
-db_password = 'EW_PUG3JR7DBeX2Z'
-db_database = 'reichstag1'
+db_host = ''
+db_port = 0
+db_user = ''
+db_password = ''
+db_database = ''
